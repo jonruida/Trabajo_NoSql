276c140ae514e967c579d8386398487f8ccee8e3
jonruida <150062541+jonruida@users.noreply.github.com>
2024-03-26 19:46:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,24 @@
-// Configurar Firebase
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    databaseURL: "https://trabajo-final-nosql-default-rtdb.europe-west1.firebasedatabase.app/",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Inicializar Firebase
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDhOogls9sWiA4KAEf-REHkN1slpvgYBg4",
+    authDomain: "trabajo-final-nosql.firebaseapp.com",
+    databaseURL: "https://trabajo-final-nosql-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "trabajo-final-nosql",
+    storageBucket: "trabajo-final-nosql.appspot.com",
+    messagingSenderId: "1029951381100",
+    appId: "1:1029951381100:web:d3fe73fcf2cdeda6f94249",
+    measurementId: "G-F1EVQ7W0PT"
+  };
 
-// Referencia a la lista de usuarios en la base de datos
-const userListRef = database.ref('users');
-
-// Manejador de envío del formulario
-document.getElementById('userForm').addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    // Obtener los datos del formulario
-    const name = document.getElementById('name').value;
-    const age = document.getElementById('age').value;
-
-    // Guardar los datos en la base de datos
-    userListRef.push({
-        name: name,
-        age: age
-    });
-
-    // Limpiar el formulario
-    document.getElementById('name').value = '';
-    document.getElementById('age').value = '';
-});
-
-// Observador de cambios en la base de datos
-userListRef.on('value', (snapshot) => {
-    const userList = document.getElementById('userList');
-    userList.innerHTML = '';
-
-    snapshot.forEach((childSnapshot) => {
-        const userData = childSnapshot.val();
-        const listItem = document.createElement('div');
-        listItem.textContent = `${userData.name}, ${userData.age} years old`;
-        userList.appendChild(listItem);
-    });
-});
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
