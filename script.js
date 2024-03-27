355ba216967f6c1e6452dd977576849cce2df3ec
jonruida <150062541+jonruida@users.noreply.github.com>
2024-03-27 15:40:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js";
+  import { getDatabase } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js";
+
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,4 +23,30 @@
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
+
+// Obtener una referencia a la colección "Usuarios" en la base de datos
+const db = firebase.database();
+const usersRef = db.ref("Usuarios");
+
+// Agregar un evento "submit" al formulario
+document.getElementById("userForm").addEventListener("submit", (event) => {
+  event.preventDefault(); // Prevenir que el formulario se envíe
+
+  // Obtener los valores del formulario
+  const nombre = document.getElementById("nombre").value;
+  const apellido = document.getElementById("apellido").value;
+  const edad = document.getElementById("edad").value;
+
+  // Enviar los datos a la base de datos
+  usersRef.push({
+    Nombre: nombre,
+    Apellido: apellido,
+    Edad: edad
+  });
+
+  // Limpiar el formulario después de enviar los datos
+  document.getElementById("nombre").value = "";
+  document.getElementById("apellido").value = "";
+  document.getElementById("edad").value = "";
+});
 </script>
