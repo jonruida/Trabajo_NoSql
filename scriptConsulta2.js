6bf99aece4444f4a63ac9352b168b8485e1adfb1
valintea <72491205+valintea@users.noreply.github.com>
2024-03-30 19:49:19 +0100

    Update scriptConsulta2.js

--- scriptConsulta2.js
+++ scriptConsulta2.js
@@ -49,7 +49,7 @@ document.getElementById("userForm").addEventListener("submit", (event) => {
           const userData = childSnapshot.val();
           if (userData.Nombre==nombre && userData.Apellidos==apellido){
             
-            alert("Nombre: "+userData.Nombre+"   Apellidos: "+userData.Apellidos+"  Genero: "+userData.Genero+"Fecha de Nacimiento: "+userData["Fecha Nacimiento"]);
+            alert("Nombre: "+userData.Nombre+"   Apellidos: "+userData.Apellidos+"  Genero: "+userData.Genero+"     Fecha de Nacimiento: "+userData["Fecha Nacimiento"]);
           }
         });
       })
